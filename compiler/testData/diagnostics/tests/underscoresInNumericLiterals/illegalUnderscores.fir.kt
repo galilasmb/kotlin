df63c5625fb23328536b79d0e76d9b3454b2a0ad
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-07-02 16:33:46 +0300

    [FIR] Add illegal underscore check in BaseFirBuilder

--- compiler/testData/diagnostics/tests/underscoresInNumericLiterals/illegalUnderscores.fir.kt
+++ compiler/testData/diagnostics/tests/underscoresInNumericLiterals/illegalUnderscores.fir.kt
@@ -1,17 +1,17 @@
 // !DIAGNOSTICS: -UNUSED_EXPRESSION
 
 fun foo() {
-    1_
-    0x_f
-    0X_f
-    0b_1
-    0B_1
-    1.0_
-    1_.1
-    1.0_e1
-    1.0E_1
-    0Xe_
-    1.1_e-1_23
-    1.0e+_0
-    1.0e-_0
+    <!ILLEGAL_UNDERSCORE!>1_<!>
+    <!ILLEGAL_UNDERSCORE!>0x_f<!>
+    <!ILLEGAL_UNDERSCORE!>0X_f<!>
+    <!ILLEGAL_UNDERSCORE!>0b_1<!>
+    <!ILLEGAL_UNDERSCORE!>0B_1<!>
+    <!ILLEGAL_UNDERSCORE!>1.0_<!>
+    <!ILLEGAL_UNDERSCORE!>1_.1<!>
+    <!ILLEGAL_UNDERSCORE!>1.0_e1<!>
+    <!ILLEGAL_UNDERSCORE!>1.0E_1<!>
+    <!ILLEGAL_UNDERSCORE!>0Xe_<!>
+    <!ILLEGAL_UNDERSCORE!>1.1_e-1_23<!>
+    <!ILLEGAL_UNDERSCORE!>1.0e+_0<!>
+    <!ILLEGAL_UNDERSCORE!>1.0e-_0<!>
 }
